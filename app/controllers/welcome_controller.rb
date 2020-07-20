a8bc2fbcef3a49e574f3502249fc56ea0bc83cba
Fabiana Munhoz <biamunhoz@gmail.com>
2020-07-20 15:52:19 -0300

    Correçoes SenhaUnica para rails 5

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,20 +1,69 @@
 class WelcomeController < ApplicationController
+
+
   def login
-    s = Senhaunica.new(ENV['ICB_OAUTH'],ENV['SECRET_OAUTH'],'2')
-    redirect_to s.login()
+
+    # Parte conceitual: http://oauthbible.com/
+    # A USP usa o OAuth 1.0a (Three Legged)
+
+    # 1. Gera objeto consumidor
+    @consumer = gera_consumidor()
+
+    # 2. Faz a primeira requisição, pegando request token/secret, que serão futuramente trocados por um access_token/secret
+    @request_token = @consumer.get_request_token
+
+    # 3. Salva o par de request token/secret gerados na sessão, pois serão usados na troca
+    session[:token] = @request_token.token
+    session[:token_secret] = @request_token.secret
+
+    # 4. Envia usuário para URL de autorização 
+    redirect_to @request_token.authorize_url + @callback_id
+
   end
 
   def callback
-    s = Senhaunica.new(ENV['ICB_OAUTH'],ENV['SECRET_OAUTH'],'2')
-    @data = s.callback(params[:oauth_verifier])
 
-    print @data
+    # 5. Gera um objeto consumidor 
+    @consumer = gera_consumidor()
+
+    # 6. Gera um hash (tipo um dicionário em python com chave/valor) com os requests token que estão na sessão (passo 3)
+    hash = { oauth_token: session[:token], oauth_token_secret: session[:token_secret]}
+
+    # 7. Constrói o objeto @request_token para fazermos a troca
+    @request_token  = OAuth::RequestToken.from_hash(@consumer, hash)
+
+    # 8. Requisita o access token, passando oauth_verifier retornado via get (Three Legged)
+    @access_token = @request_token.get_access_token(oauth_verifier: params[:oauth_verifier])
+
+    # 9. Com posse do access token, faça uma requisição POST
+    json_response = @access_token.post("https://uspdigital.usp.br/wsusuario/oauth/usuariousp");
+    @data = JSON.parse(json_response.body)
+
+    #10. @data tem os dados 
 
     loginUsuario = tratauser
 
     log_in loginUsuario
 
   end
+
+  def gera_consumidor
+
+    @callback_id = "&callback_id=" + ENV["ID_OAUTH"]
+    key = ENV["ICB_OAUTH"]
+    secret = ENV["SECRET_OAUTH"]
+   
+
+    return OAuth::Consumer.new(key, secret, {
+                                           :site               => "https://uspdigital.usp.br",
+                                           :scheme             => :header,
+                                           :http_method        => :post,
+                                           :request_token_path => "/wsusuario/oauth/request_token",
+                                           :access_token_path  => "/wsusuario/oauth/access_token",
+                                           :authorize_path     => "/wsusuario/oauth/authorize"
+                                       })
+  end
+
 
   def tratauser
 
@@ -74,7 +123,7 @@ class WelcomeController < ApplicationController
 
   def destroy
 
-    log_lout
+    log_out
     redirect_to root_path
   end
   
