aa1a64e83486e23026489251857601c6e374205f
Fabiana Munhoz <biamunhoz@gmail.com>
2020-11-24 10:05:38 -0300

    Permissao para admin

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -3,6 +3,20 @@ module WelcomeHelper
   #cria uma sessão
   def log_in(loginUsuario)
     session[:login] = loginUsuario
+    session[:admingeral] = false
+
+    @user = Usuario.find_by(:loginUsuario => loginUsuario)
+
+    @vinculo = TipoVinculo.where(:usuario_id => @user.id)
+
+    @vinculo.each do |v|
+      print v.codigoSetor
+      if v.codigoSetor.to_s() == '3610'
+        session[:admingeral] = true
+      end 
+    end
+    
+
   end
 
   #destroi a sessão criada na def anterior
