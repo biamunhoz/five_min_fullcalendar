5fefb3f198f75547b0af1d6e8a96c95f0157ffdd
Fabiana Rodrigues <biamunhoz@gmail.com>
2021-05-21 10:31:13 -0300

    Ajustes gerais de tela e seed com perfil

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Perfil.create(nomeperfil: "Admin");
+Perfil.create(nomeperfil: "Supervisor");
+Perfil.create(nomeperfil: "Simples");
