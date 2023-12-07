d08d03efce38b86b3745ecf03d8ba675a0a03995
Fabiana M. M. Rodrigues <biamunhoz@gmail.com>
2023-12-07 15:39:14 -0300

    Apresentação do nome na listagem

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,14 @@
 module EventsHelper
 
-
+    def extrair_primeiro_nome(nome_completo)
+        # Divida o nome completo em palavras usando o espaço como delimitador
+        palavras = nome_completo.split
+        
+        # O primeiro nome é a primeira palavra da lista
+        primeiro_nome = palavras.first
+        
+        return primeiro_nome
+      end
+      
 
 end
